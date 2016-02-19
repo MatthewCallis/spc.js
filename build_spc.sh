1d1347dd7cf0627aa39e760a8b708ab27d15c0f5
Matthew Callis <matthew.callis@gmail.com>
2016-02-19 14:42:57 -0800

    Clean up build with notes

--- build_spc.sh
+++ build_spc.sh
@@ -11,10 +11,23 @@
 mkdir -p dist;
 
 # TODO: Test these
-# -s ALLOW_MEMORY_GROWTH=1 \
-# -s AGGRESSIVE_VARIABLE_ELIMINATION=1 \
 # --profiling \
 # -s ERROR_ON_UNDEFINED_SYMBOLS=1 \
+# -s ALLOW_MEMORY_GROWTH=1 \
+# -s NO_FILESYSTEM=1 \
+# -s NO_BROWSER=1 \
+
+# NOTE: http://kripken.github.io/emscripten-site/docs/optimizing/Optimizing-Code.html#miscellaneous-code-size-tips
+# This currently results in: "spc-aurora.js:2481 Uncaught ReferenceError: Module is not defined"
+# --closure 1 \
+
+# NOTE: http://kripken.github.io/emscripten-site/docs/optimizing/Optimizing-Code.html#aggressive-variable-elimination
+# This increased the size by 5KB. I don't believe the core has large enough functions for this to be useful to SPC.js.
+# -s AGGRESSIVE_VARIABLE_ELIMINATION=1 \
+
+# NOTE: http://kripken.github.io/emscripten-site/docs/optimizing/Optimizing-Code.html#memory-initialization
+# This embeds the .mem files, so we do not need to include the .mem file seperately.
+# --memory-init-file 0
 
 # Build spc.js
 emcc -v
@@ -27,7 +40,9 @@ emcc \
   -s ASM_JS=1 \
   -s WARN_ON_UNDEFINED_SYMBOLS=1 \
   -s NO_EXIT_RUNTIME=1 \
+  -s NO_FILESYSTEM=1 \
   -s EXPORTED_FUNCTIONS="['_SpcJsInit', '_SpcJsDestroy', '_SpcJsDecodeAudio']" \
+  --memory-init-file 0 \
   -Iinclude \
   include/snes_spc/*.cpp \
   src/spc-libs.c \
